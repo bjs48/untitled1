264bb544739c46342fc678027209926c3f4aaa67
balaji <bjs48@njit.edu>
2018-03-26 12:56:38 -0400

    modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 1git
                 </div>
 
                 <div class="links">
